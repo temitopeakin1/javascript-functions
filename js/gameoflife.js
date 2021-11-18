4b5b73e17c4d3948b6efed96fbcb581899fe8263
Akinmegha Temitope Samuel <temitopeakinmegha@gmail.com>
2021-11-18 11:04:03 +0100

    Completed module

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -54,10 +54,14 @@ const printCells = (state) =>
   return accumulator;
 };
 
+
+
 const getNeighborsOf = ([x, y]) => [
   [x - 1, y + 1], [x, y + 1], [x + 1, y + 1],
-  [x - y, y], [x + 1, y], [x - 1, y - 1], [x, y - 1], [x + 1, y - 1]
+  [x - y, y],                     [x + 1, y],
+  [x - 1, y - 1], [x, y - 1], [x + 1, y - 1]
 ];
+
 
 const getLivingNeighbors = (cell, state) =>
 {
